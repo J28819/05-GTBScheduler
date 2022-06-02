579f85ef0699b60acba33d7815f642f69855536b
BC_IML <lelj28819@gmail.com>
2022-06-02 16:32:19 -0400

    Customizing the CSS for Add Overtime

--- Assets/js/scrollIntoView.js
+++ Assets/js/scrollIntoView.js
@@ -16,6 +16,14 @@ element.addEventListener("scroll", function(){ // or window.addEventListener("sc
    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
 }, false);
 
-//const list = document.getElementById("list");
+function appendText() {
+   var txt2 = $(`<li>
+                  <div id="${idrow}" class="row time-block">
+                     <p class="col-md-1 hour">6am</p>
+                     <textarea class="col-md-8 description" placeholder="Write here your activities"></textarea>
+                     <button class="col-md-1 btn saveBtn"><i class="glyphicon glyphicon-floppy-disk" style="font-size:36px;"></i></button>
+                  </div>
+                 </li>`);   // Create with jQuery
 
-//list.innerHTML += `<li><a href="#">Item ${list.children.length + 1}</a></li>`;
+   $("ul").append(txt2);      // Append the new elements 
+ }
