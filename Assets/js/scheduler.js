0ceca8fbcd967f03d1910bad7cce946051d1ab21
BC_IML <lelj28819@gmail.com>
2022-06-04 11:49:52 -0400

    Adding additional code for scheduler Structure and make it flex for movile devices

---
+++ Assets/js/scheduler.js
@@ -0,0 +1,99 @@
+//var date = moment().format('MMMM Do YYYY, h:mm:ss a')
+var date = moment().format('MMMM Do YYYY')
+var hour = moment().format('h')
+$("#timeFinder").val(date);
+
+ObjectSquema = [ 
+    {"date": date,
+    "data": [
+            {"time":6,
+            "msg": " "},
+            {"time":7,
+            "msg": " "},
+            {"time":8,
+            "msg": " "},
+            {"time":9,
+            "msg": " "},
+            {"time":10,
+            "msg": " "},
+            {"time":11,
+            "msg": " "},
+            {"time":12,
+            "msg": " "},
+            {"time":13,
+            "msg": " "},
+            {"time":14,
+            "msg": " "},
+            {"time":15,
+            "msg": " "},
+            {"time":16,
+            "msg": " "},
+            {"time":17,
+            "msg": " "},
+            {"time":18,
+            "msg": " "}
+            ]
+    }    
+]
+
+
+$(document)
+
+ObjectSquema[0]['data'].forEach(element => {
+    var Objtime = element['time']
+
+    var lastNumber = $(ObjectSquema).get(-1)
+    console.log(lastNumber)
+    if (Objtime < 12) {
+        var timeini = moment().format(Objtime + ':00 a')
+    } else{
+        var timeini = moment().format(Objtime + ':00 a')
+    }
+    
+    elementsdefault = `<li>
+    <div id="newItem" class="row time-block">
+       <p class="col-md-1 hour">${timeini}</p>
+        <textarea class="col-md-8 description" placeholder="Write here your activities"></textarea>
+           <button class="col-md-1 btn saveBtn"><i class="glyphicon glyphicon-floppy-disk" style="font-size:36px;"></i></button>
+       </div>`
+    $("#ulList").append(elementsdefault);
+});
+
+console.log(ObjectSquema)
+// Adding Overtime blocks to the list
+    $("#addTime").click(function(){
+
+        var arraylength = ObjectSquema[0]['data'];
+        
+        var superv = arraylength.length
+        console.log( $( "#ulList li" ).get( -1 ) )
+        var newtime = superv + 1 
+        //console.log(newtime)
+        var time = " "
+        if (newtime < 12) {
+            time = moment().format(newtime + ':00 a')
+        } else {
+            time = moment().format(newtime + ':00 a')
+        }
+        
+
+        var txt2 = `<li>
+        <div id="newItem" class="row time-block">
+           <p class="col-md-1 hour">${time}</p>
+            <textarea class="col-md-8 description" placeholder="Write here your activities"></textarea>
+               <button class="col-md-1 btn saveBtn"><i class="glyphicon glyphicon-floppy-disk" style="font-size:36px;"></i></button>
+           </div>`
+
+        $("#ulList").append(txt2);
+        //Scroll down after add the element in the List
+        $("html, body").animate({
+            scrollTop: $(
+              'html, body').get(0).scrollHeight
+        }, 100);
+       
+    });
+
+
+   
+
+    
--- Assets/js/scrollIntoView.js
+++ Assets/js/scrollIntoView.js
@@ -1,29 +1,13 @@
-var lastScrollTop = 0;
-window.scrollTo(300, 500);	//X=300 and Y=500
-var element = document.querySelector("#scroll1");
-// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
-element.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
-   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
-   console.log("scrolling")
-   
-   if (st > lastScrollTop){
-      // downscroll code
-      console.log("scrolling down")
-   } else {
-      // upscroll code
-      console.log("scrolling up")
-   }
-   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-}, false);
+console.log("ScrollIntoView.js")
 
-function appendText() {
-   var txt2 = $(`<li>
-                  <div id="${idrow}" class="row time-block">
-                     <p class="col-md-1 hour">6am</p>
-                     <textarea class="col-md-8 description" placeholder="Write here your activities"></textarea>
-                     <button class="col-md-1 btn saveBtn"><i class="glyphicon glyphicon-floppy-disk" style="font-size:36px;"></i></button>
-                  </div>
-                 </li>`);   // Create with jQuery
+//function appendText() {
+  // var txt2 = $(`<li>
+                //  <div id="${idrow}" class="row time-block">
+                  //   <p class="col-md-1 hour">6am</p>
+                   //  <textarea class="col-md-8 description" placeholder="Write here your activities"></textarea>
+                   //  <button class="col-md-1 btn saveBtn"><i class="glyphicon glyphicon-floppy-disk" style="font-size:36px;"></i></button>
+                //  </div>
+               //  </li>`);   // Create with jQuery
 
-   $("ul").append(txt2);      // Append the new elements 
- }
+  // $("ul").append(txt2);      // Append the new elements 
+ //}
