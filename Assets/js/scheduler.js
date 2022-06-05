afc2d63fd86586edaa541cc165d921f166d64e58
BC_IML <lelj28819@gmail.com>
2022-06-05 09:42:36 -0400

    fix bug for past present and future and clean code

--- Assets/js/scheduler.js
+++ Assets/js/scheduler.js
@@ -1,14 +1,14 @@
-//var date = moment().format('MMMM Do YYYY, h:mm:ss a')
+//Global variables
 var date = moment().format('MMMM Do YYYY')
 var hour = moment().format('h')
 var variabledate = (moment().add(1, 'days').format('MMMM Do YYYY'))
 
 $("#timeFinder").text(date);
-console.log(variabledate)
+
 
 // Check if the Local Storage is Empty if True, Write the empty Object if not Load the Existing Elements
     Localstoragevar = localStorage.getItem(date);
-    console.log(Localstoragevar)
+    
     if (Localstoragevar !==null){
          ObjectSquema = JSON.parse(Localstoragevar)
     }else{
@@ -46,19 +46,7 @@ console.log(variabledate)
         ]
         localStorage.setItem(date.toString(),  JSON.stringify(ObjectSquema));
     }
-   
-
-if (variabledate === date){
-    console.log("yes is the same")
-    console.log(date)
-
-}else{
-    console.log("no is not the same")
-}
-
-
-
-
+// Create list of elements to display    
 ObjectSquema[0]['data'].forEach(element => {
     var Objtime = element['time']
     var Objmsg= element['msg']
@@ -67,9 +55,10 @@ ObjectSquema[0]['data'].forEach(element => {
     if (Objtime < 12) {
         var timeini = (Objtime + ':00' + " am")
     } else{
+        
         var timeini = (Objtime + ':00' + " pm")
     }
-    
+   
     elementsdefault = `<li>
     <div id="nodata" class="row time-block">
        <p class="col-md-1 hour">${timeini}</p>
@@ -77,31 +66,52 @@ ObjectSquema[0]['data'].forEach(element => {
            <button class="col-md-1 btn saveBtn"><i class="glyphicon glyphicon-floppy-disk" style="font-size:36px;"></i></button>
        </div>`
     $("#ulList").append(elementsdefault);
+    checktime();
 });
 
-//console.log(ObjectSquema)
-// Adding Overtime blocks to the list
+
+
+// function to check if is past, present or future and add attributes
+function checktime(){
+    var listItems = $("#ulList li");
+listItems.each(function(idx, li) {
+    var activity = $(li);
+    var datax = $(".description", activity).data('x')
+    if (datax == hour){
+        $(".description", activity).attr("style", "background-color: #d3d3d3" )
+    }
+    if (datax < hour){
+        $(".description", activity).attr("style", "background-color: #ff6961") 
+    }
+    if (datax > hour){
+        $(".description", activity).attr("style", "background-color: #77dd77")
+    }
+
+});
+ }
+
+ checktime();
+
+
     $("#addTime").click(function(){
 
         var arraylength = ObjectSquema[0]['data'];
         let offset = 6
-
-       
         var newtime = (arraylength.length + offset)
         var time = " "
         if (newtime <= 23) {
-
-        
         if (newtime < 12) {
             time = (newtime + ':00' + ' am');
             ObjectSquema[0]['data'].push({"time":newtime, "msg": " "})
             localStorage.setItem(date.toString(),  JSON.stringify(ObjectSquema));
         } else {
+            newtime = newtime - 12
             time = (newtime + ':00' + ' pm')
             ObjectSquema[0]['data'].push({"time":newtime, "msg": " "})
             localStorage.setItem(date.toString(),  JSON.stringify(ObjectSquema));
         }
-        
+
+       
         var txt2 = `<li>
         <div id="newItem" class="row time-block">
            <p class="col-md-1 hour">${time}</p>
@@ -115,29 +125,23 @@ ObjectSquema[0]['data'].forEach(element => {
             scrollTop: $(
               'html, body').get(0).scrollHeight
         }, 100); 
+        checktime();
     }
     });
 
+    // Save Button Function
     $(".saveBtn").click(function(){
-        console.log("Save Button Pressted")
-        //var textarea = $(this).siblings()
         var textarea = $(this).siblings(".description").val()
         var id = $(this).siblings(".description").data('x')
-
         Localstorage = localStorage.getItem(date);
         ObjectSquema = JSON.parse(Localstorage);
          (ObjectSquema[0].data).forEach(element => {
              if (element.time === id){
                  element.msg = textarea;
-                 console.log(element)
-                 
              }
-
-         }
-         
-
+            }
          )
-         console.log(ObjectSquema[0].data)
+         
         localStorage.setItem(date.toString(),  JSON.stringify(ObjectSquema));
         //location.reload();
     })
