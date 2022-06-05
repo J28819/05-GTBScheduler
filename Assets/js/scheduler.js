0da913ce47b3e37056c05a05a097109c8e603dac
BC_IML <lelj28819@gmail.com>
2022-06-04 22:38:43 -0400

    BugFix for create items in list

--- Assets/js/scheduler.js
+++ Assets/js/scheduler.js
@@ -1,85 +1,111 @@
 //var date = moment().format('MMMM Do YYYY, h:mm:ss a')
 var date = moment().format('MMMM Do YYYY')
 var hour = moment().format('h')
-$("#timeFinder").val(date);
+var variabledate = (moment().add(1, 'days').format('MMMM Do YYYY'))
 
-ObjectSquema = [ 
-    {"date": date,
-    "data": [
-            {"time":6,
-            "msg": " "},
-            {"time":7,
-            "msg": " "},
-            {"time":8,
-            "msg": " "},
-            {"time":9,
-            "msg": " "},
-            {"time":10,
-            "msg": " "},
-            {"time":11,
-            "msg": " "},
-            {"time":12,
-            "msg": " "},
-            {"time":13,
-            "msg": " "},
-            {"time":14,
-            "msg": " "},
-            {"time":15,
-            "msg": " "},
-            {"time":16,
-            "msg": " "},
-            {"time":17,
-            "msg": " "},
-            {"time":18,
-            "msg": " "}
-            ]
-    }    
-]
+$("#timeFinder").text(date);
+console.log(variabledate)
+
+// Check if the Local Storage is Empty if True, Write the empty Object if not Load the Existing Elements
+    Localstoragevar = localStorage.getItem(date);
+    console.log(Localstoragevar)
+    if (Localstoragevar !==null){
+         ObjectSquema = JSON.parse(Localstoragevar)
+    }else{
+        ObjectSquema = [ 
+            {"date": date,
+            "data": [
+                    {"time":6,
+                    "msg": " "},
+                    {"time":7,
+                    "msg": " "},
+                    {"time":8,
+                    "msg": " "},
+                    {"time":9,
+                    "msg": " "},
+                    {"time":10,
+                    "msg": " "},
+                    {"time":11,
+                    "msg": " "},
+                    {"time":12,
+                    "msg": " "},
+                    {"time":13,
+                    "msg": " "},
+                    {"time":14,
+                    "msg": " "},
+                    {"time":15,
+                    "msg": " "},
+                    {"time":16,
+                    "msg": " "},
+                    {"time":17,
+                    "msg": " "},
+                    {"time":18,
+                    "msg": " "}
+                    ]
+            }    
+        ]
+        localStorage.setItem(date.toString(),  JSON.stringify(ObjectSquema));
+    }
+   
+
+if (variabledate === date){
+    console.log("yes is the same")
+    console.log(date)
+
+}else{
+    console.log("no is not the same")
+}
 
 
-$(document)
+
 
 ObjectSquema[0]['data'].forEach(element => {
     var Objtime = element['time']
 
     var lastNumber = $(ObjectSquema).get(-1)
-    console.log(lastNumber)
+   
     if (Objtime < 12) {
-        var timeini = moment().format(Objtime + ':00 a')
+        var timeini = (Objtime + ':00' + " am")
     } else{
-        var timeini = moment().format(Objtime + ':00 a')
+        var timeini = (Objtime + ':00' + " pm")
     }
     
     elementsdefault = `<li>
-    <div id="newItem" class="row time-block">
+    <div id="nodata" class="row time-block">
        <p class="col-md-1 hour">${timeini}</p>
-        <textarea class="col-md-8 description" placeholder="Write here your activities"></textarea>
+        <textarea data-x="${Objtime}" class="col-md-8 description" placeholder="Write here your activities"></textarea>
            <button class="col-md-1 btn saveBtn"><i class="glyphicon glyphicon-floppy-disk" style="font-size:36px;"></i></button>
        </div>`
     $("#ulList").append(elementsdefault);
 });
 
-console.log(ObjectSquema)
+//console.log(ObjectSquema)
 // Adding Overtime blocks to the list
     $("#addTime").click(function(){
 
         var arraylength = ObjectSquema[0]['data'];
-        
-        var superv = arraylength.length
-        console.log( $( "#ulList li" ).get( -1 ) )
-        var newtime = superv + 1 
-        //console.log(newtime)
+        let offset = 6
+
+       
+        var newtime = (arraylength.length + offset)
         var time = " "
+        if (newtime <= 23) {
+
+        
         if (newtime < 12) {
-            time = moment().format(newtime + ':00 a')
+            time = (newtime + ':00' + ' am');
+            ObjectSquema[0]['data'].push({"time":newtime, "msg": " "})
+            localStorage.setItem(date.toString(),  JSON.stringify(ObjectSquema));
         } else {
-            time = moment().format(newtime + ':00 a')
+            time = (newtime + ':00' + ' pm')
+            ObjectSquema[0]['data'].push({"time":newtime, "msg": " "})
+            localStorage.setItem(date.toString(),  JSON.stringify(ObjectSquema));
         }
         
         var txt2 = `<li>
         <div id="newItem" class="row time-block">
            <p class="col-md-1 hour">${time}</p>
-            <textarea class="col-md-8 description" placeholder="Write here your activities"></textarea>
+            <textarea data-x="${newtime}" class="col-md-8 description" placeholder="Write here your activities"></textarea>
                <button class="col-md-1 btn saveBtn"><i class="glyphicon glyphicon-floppy-disk" style="font-size:36px;"></i></button>
            </div>`
 
@@ -88,9 +114,33 @@ console.log(ObjectSquema)
         $("html, body").animate({
             scrollTop: $(
               'html, body').get(0).scrollHeight
-        }, 100);
-       
+        }, 100); 
+    }
     });
+
+    $(".saveBtn").click(function(){
+        console.log("Save Button Pressted")
+        //var textarea = $(this).siblings()
+        var textarea = $(this).siblings(".description").val()
+        var id = $(this).siblings(".description").data('x')
+
+        var LocalstorageUpdate = localStorage.getItem(date);
+        var ObjectSquemaUpdate = JSON.parse(LocalstorageUpdate);
+         (ObjectSquemaUpdate[0].data).forEach(element => {
+             if (element.time === id){
+                 element.msg = textarea;
+                 console.log(element)
+                 
+             }
+
+         }
+         
+
+         )
+         console.log(ObjectSquemaUpdate[0].data)
+        localStorage.setItem(date.toString(),  JSON.stringify(ObjectSquemaUpdate));
+        location.reload();
+    })
 
 
    
